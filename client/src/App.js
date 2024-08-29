ace7f0947d1cdbe64e7847136ff7c39ee6c10a70
yunzhi chen <yunzhichen@yunzhis-Laptop.local>
2024-08-29 18:19:08 +0800

    Add Navbar and HomePage components

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
