10f2729a581824f0a7cd8e83fe9e74919a288665
WannianMEI <M20211121@126.com>
2024-09-29 13:28:30 +0800

    Remove node_modules

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -19,25 +19,25 @@ const DashboardPage = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      mode: 'cors',  
+      mode: 'cors',
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); 
-    })
-    .then(data => {
-      console.log('Received data from backend:', data); 
-      setDashboardData({
-        totalTasks: data.totalTasks || 0,
-        toDoTasks: data.toDoTasks || 0,
-        inProgressTasks: data.inProgressTasks || 0,
-        completedTasks: data.completedTasks || 0,
-      });
-    })  
-    .catch(error => console.error('Error fetching data:', error)); 
-  }, []);  
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Received data from backend:', data);
+          setDashboardData({
+            totalTasks: data.totalTasks || 0,
+            toDoTasks: data.toDoTasks || 0,
+            inProgressTasks: data.inProgressTasks || 0,
+            completedTasks: data.completedTasks || 0,
+          });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   const dataPie = [
     { name: 'Completed', value: dashboardData.completedTasks },
@@ -52,71 +52,71 @@ const DashboardPage = () => {
   ];
 
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-content">
-        <h1>PMS DASHBOARD</h1>
-        <div className="task-cards">
-          <div className="task-card">
-            <h2>COMPLETED TASKS</h2>
-            <p>{dashboardData.completedTasks}</p>
-            <span>Task count</span>
+      <div className="dashboard-container">
+        <div className="dashboard-content">
+          <h1>PMS DASHBOARD</h1>
+          <div className="task-cards">
+            <div className="task-card">
+              <h2>COMPLETED TASKS</h2>
+              <p>{dashboardData.completedTasks}</p>
+              <span>Task count</span>
+            </div>
+            <div className="task-card">
+              <h2>INCOMPLETED TASKS</h2>
+              <p>{dashboardData.inProgressTasks}</p>
+              <span>Task count</span>
+            </div>
+            <div className="task-card">
+              <h2>OVERDUE TASKS</h2>
+              <p>{dashboardData.toDoTasks}</p>
+              <span>Task count</span>
+            </div>
+            <div className="task-card">
+              <h2>TOTAL TASKS</h2>
+              <p>{dashboardData.totalTasks}</p>
+              <span>Task count</span>
+            </div>
           </div>
-          <div className="task-card">
-            <h2>INCOMPLETED TASKS</h2>
-            <p>{dashboardData.inProgressTasks}</p>
-            <span>Task count</span>
-          </div>
-          <div className="task-card">
-            <h2>OVERDUE TASKS</h2>
-            <p>{dashboardData.toDoTasks}</p>
-            <span>Task count</span>
-          </div>
-          <div className="task-card">
-            <h2>TOTAL TASKS</h2>
-            <p>{dashboardData.totalTasks}</p>
-            <span>Task count</span>
-          </div>
-        </div>
-        <div className="charts">
-          <div className="chart">
-            <h2>ALL TASKS BY COMPLETION STATUS</h2>
-            <PieChart width={250} height={250}>
-              <Pie
-                data={dataPie}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
+          <div className="charts">
+            <div className="chart">
+              <h2>ALL TASKS BY COMPLETION STATUS</h2>
+              <PieChart width={250} height={250}>
+                <Pie
+                    data={dataPie}
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                >
+                  {dataPie.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </div>
+            <div className="chart">
+              <h2>INCOMPLETED TASKS BY GROUP</h2>
+              <BarChart
+                  width={350}
+                  height={250}
+                  data={dataBar}
+                  margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                  }}
               >
-                {dataPie.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-            </PieChart>
-          </div>
-          <div className="chart">
-            <h2>INCOMPLETED TASKS BY GROUP</h2>
-            <BarChart
-              width={350}
-              height={250}
-              data={dataBar}
-              margin={{
-                top: 5, right: 30, left: 20, bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="tasks" fill="#82ca9d" />
-            </BarChart>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="tasks" fill="#82ca9d" />
+              </BarChart>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
